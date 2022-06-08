05a41d255e6dd4d9838738313b320f55a48de8ca
James Coates <69493073+nexuspcs@users.noreply.github.com>
2022-06-09 00:49:42 +1000

    Added description

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -34,7 +34,7 @@ struct ContentView: View {
 
                 Text("About Pittwater")
                     .font(.title2)
-                Text("Descriptive text goes here.")
+                Text("Pittwater is a semi-mature tide dominated drowned valley estuary, located about 40 kilometres north of the Sydney central business district, New South Wales, Australia; being one of the bodies of water that separate greater Metropolitan Sydney from the Central Coast. Pittwater has its origin from the confluence of McCarrs Creek, to the west of Church Point and a number of smaller estuaries, the largest of which is Cahill Creek, that joins the Pittwater north of Mona Vale. The Pittwater is an open body of water, often considered a bay or harbour, that flows north towards its mouth into Broken Bay, between West Head and Barrenjoey Head, less than 1 kilometre from the Tasman Sea.")
             }
             .padding()
 
